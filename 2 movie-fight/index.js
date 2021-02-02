24200fbfc5028274f8b36ba07bfb8dff7ed646c1
Sonal Agrawal <37601228+coolsonu39@users.noreply.github.com>
2021-02-03 00:27:02 +0530

    Update index.js

--- 2 movie-fight/index.js
+++ 2 movie-fight/index.js
@@ -9,7 +9,7 @@ const autocompletConfig = {
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get("http://www.omdbapi.com/", {
+        const response = await axios.get("https://www.omdbapi.com/", {
             params: {
                 // replace with real key
                 apikey: '92d301c9',
@@ -42,7 +42,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             // replace with real key
             apikey: '92d301c9',
